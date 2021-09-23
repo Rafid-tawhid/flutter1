f3c486b146481c3916912f3c07a8e55a113d643d
Tawhidur Rahman Rafid <55914303+Rafid-tawhid@users.noreply.github.com>
2021-09-24 03:03:26 +0600

    Splash Screen and callback

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,62 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(myApp());
 }
-
 class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Fields',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      title: "Material App",
+      home: HomePage(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String myvar="Default";
-  String show="Default";
-  void showText()
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    debugPrint("Starteed Splash Screen");
+    Timer(Duration(seconds: 5),finished);
+  }
+
+  void finished()
   {
-    setState(() {
-      show=myvar;
-    });
+    debugPrint("Finished");
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Text("$show"),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              onChanged: (text) {
-                myvar = text;
-              },
-              maxLength: 18,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.account_balance,
-                    color: Colors.blue,
-                  ),
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  labelStyle: TextStyle(
-                    color: Colors.deepOrange,
-                    fontSize: 18,
-                  )),
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 18.0,
-              ),
-            ),
-          ),
-          RaisedButton(onPressed: showText,
-          child: Text(
-            "Press ",
-          ),)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Splash\nScreen",style: TextStyle(fontSize: 40,color: Colors.white,fontFamily: "Cursive"),),
+
+            ],
+          )
         ],
       ),
     );
   }
 }
+
+
+
