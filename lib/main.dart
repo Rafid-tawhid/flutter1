c110ff43e4b661c630d37ad566c39e0cb523737f
Tawhidur Rahman Rafid <55914303+Rafid-tawhid@users.noreply.github.com>
2021-09-24 03:50:53 +0600

    Calculator app with design

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(myApp());
 }
+
 class myApp extends StatelessWidget {
   const myApp({Key? key}) : super(key: key);
 
@@ -12,11 +12,12 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.deepOrange),
-      title: "Material App",
+      title: "Calculator App",
       home: HomePage(),
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -25,38 +26,128 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int firstNum=0;
+  int secondNum=0;
+  String display="";
+  String res="";
+  String operator="";
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    debugPrint("Starteed Splash Screen");
-    Timer(Duration(seconds: 5),finished);
-  }
-
-  void finished()
+  void btnClicked(String v)
   {
-    debugPrint("Finished");
+    if(v=="C")
+      {
+        display="";
+        firstNum=0;
+        secondNum=0;
+        res="";
+
+      }
+    else if(v=="+"||v=="-"||v=="/"||v=="x")
+      {
+        firstNum=int.parse(display);
+        res="";
+        operator=v;
+      }
+    else if (v=="=")
+      {
+        secondNum=int.parse(display);
+        if(operator=="+")
+          {
+            res=(firstNum+secondNum).toString();
+          }
+        if(operator=="-")
+        {
+          res=(firstNum-secondNum).toString();
+        }
+        if(operator=="x")
+        {
+          res=(firstNum*secondNum).toString();
+        }
+        if(operator=="/")
+        {
+          res=(firstNum/secondNum).toString();
+        }
+      }
+    else
+      {
+        res=int.parse(display+v).toString();
+      }
+    setState(() {
+      display=res;
+    });
   }
+
+
+  Widget custombutton(String val) {
+    return Expanded(
+        child: OutlinedButton(
+      onPressed: () => btnClicked(val),
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Text(
+          "$val",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Splash\nScreen",style: TextStyle(fontSize: 40,color: Colors.white,fontFamily: "Cursive"),),
-
-            ],
-          )
-        ],
+      appBar: AppBar(
+        title: Text("Calculator"),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+                child: Container(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20,right: 20),
+                child: Text(
+                  "$display",
+                  style: TextStyle(fontSize: 40, fontFamily: "Cursive"),
+                ),
+              ),
+            )),
+            Row(
+              children: [
+                custombutton("9"),
+                custombutton("8"),
+                custombutton("7"),
+                custombutton("+"),
+              ],
+            ),
+            Row(
+              children: [
+                custombutton("6"),
+                custombutton("5"),
+                custombutton("4"),
+                custombutton("-"),
+              ],
+            ),
+            Row(
+              children: [
+                custombutton("3"),
+                custombutton("2"),
+                custombutton("1"),
+                custombutton("x"),
+              ],
+            ),
+            Row(
+              children: [
+                custombutton("C"),
+                custombutton("0"),
+                custombutton("="),
+                custombutton("/"),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
